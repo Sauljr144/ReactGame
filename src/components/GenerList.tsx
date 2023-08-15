42c7d772fc7055d69e7745c67a9f786e414b58d9
Sauljr144 <saulsuazojr06@gmail.com>
2023-08-14 19:50:32 -0700

    Added list from chakra and imgs for genre side bar

--- src/components/GenerList.tsx
+++ src/components/GenerList.tsx
@@ -1,5 +1,7 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useData from "../hooks/useData";
 import useGenres, { Genre } from "../hooks/useGenres"
+import getCroppedImageUrl from "../services/image-url";
 
 
 const GenerList = () => {
@@ -7,9 +9,14 @@ const GenerList = () => {
     const {data} = useData<Genre>('/genres');
 
   return (
-    <ul>
-        {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-    </ul>
+    <List>
+        {data.map(genre => <ListItem key={genre.id} paddingY='5px'>
+            <HStack>
+                <Image boxSize='32px' borderRadius={8}src={getCroppedImageUrl(genre.image_background)} />
+                <Text fontSize='lg'>{genre.name}</Text>
+            </HStack>
+        </ListItem>)}
+    </List>
   )
 }
 
